8c463418a707471cd963e356f8d243422bac62fc
Anna Nguyen <anna-nguyen@berkeley.edu>
2019-10-22 16:14:19 -0700

    lab7 3.6 fix

--- materials/fa19/lab/lab07/tests/q3_6.py
+++ materials/fa19/lab/lab07/tests/q3_6.py
@@ -14,16 +14,26 @@ test = {
         },
         {
           'code': r"""
-          >>> rate_means.where("Death Penalty", "False").column(1).item(0)
-          8.120454540452272
+          >>> test = False;
+          >>> try:
+          ...     test = round(rate_means.where("Death Penalty", "False").column(1).item(0), 3) == 8.12;
+          >>> except:
+          ...     test = round(rate_means.where("Death Penalty", False).column(1).item(0), 3) == 8.12;
+          >>> test
+          True
           """,
           'hidden': False,
           'locked': False
         },
         {
           'code': r"""
-          >>> rate_means.where("Death Penalty", "True").column(1).item(0)
-          7.513636380386362
+          >>> test = False;
+          >>> try:
+          ...     test = round(rate_means.where("Death Penalty", "True").column(1).item(0), 3) == 7.514;
+          >>> except:
+          ...     test = round(rate_means.where("Death Penalty", True).column(1).item(0), 3) == 7.514;
+          >>> test
+          True
           """,
           'hidden': False,
           'locked': False
