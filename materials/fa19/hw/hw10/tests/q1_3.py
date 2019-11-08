71a44643bca92240209756422a87df6f282728eb
Anna Nguyen <anna-nguyen@berkeley.edu>
2019-11-08 12:03:29 -0800

    hw 10

---
+++ materials/fa19/hw/hw10/tests/q1_3.py
@@ -0,0 +1,30 @@
+test = {
+  'name': 'q1_3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> type(lower_bound) in set([float, np.float32, np.float64])
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> type(upper_bound) in set([float, np.float32, np.float64])
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
